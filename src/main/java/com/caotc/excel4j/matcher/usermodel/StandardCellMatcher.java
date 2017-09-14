85557a40d02194a6fde03bdd09667a8e069c0ac5
caotc <250622148@qq.com>
2017-09-14 23:49:17 +0800

    删除冗余方法

--- src/main/java/com/caotc/excel4j/matcher/usermodel/StandardCellMatcher.java
+++ src/main/java/com/caotc/excel4j/matcher/usermodel/StandardCellMatcher.java
@@ -1,23 +1,25 @@
 package com.caotc.excel4j.matcher.usermodel;
 
 import java.util.Collection;
-
+import org.apache.poi.hssf.util.CellReference;
 import com.caotc.excel4j.matcher.data.DataMatcher;
 import com.caotc.excel4j.matcher.data.value.ComparableValueMatcher;
 import com.caotc.excel4j.matcher.data.value.StringMatcher;
 import com.caotc.excel4j.parse.result.StandardCell;
-import com.caotc.excel4j.util.ExcelUtil;
 import com.caotc.excel4j.util.MatcherUtil;
 
-public class StandardCellMatcher{
-	private DataMatcher valueMatcher;
-	private Collection<ComparableValueMatcher<Integer>> rowNumberMatchers;
-	private Collection<ComparableValueMatcher<Integer>> columnNumberMatchers;
-	private Collection<StringMatcher> columnStringMatchers;
-	
-	public boolean matches(StandardCell cell){
-		return valueMatcher.matches(cell.getValue()) && MatcherUtil.allMatches(rowNumberMatchers
-				, cell.getValueCell().getRowIndex()) && MatcherUtil.allMatches(columnNumberMatchers, cell.getValueCell().getColumnIndex()) 
-				&& MatcherUtil.allMatches(columnStringMatchers, ExcelUtil.indexToColumn(cell.getValueCell().getColumnIndex()));
-	}
+public class StandardCellMatcher {
+    private DataMatcher valueMatcher;
+    private Collection<ComparableValueMatcher<Integer>> rowNumberMatchers;
+    private Collection<ComparableValueMatcher<Integer>> columnNumberMatchers;
+    private Collection<StringMatcher> columnStringMatchers;
+
+    public boolean matches(StandardCell cell) {
+        return valueMatcher.matches(cell.getValue())
+                && MatcherUtil.allMatches(rowNumberMatchers, cell.getValueCell().getRowIndex())
+                && MatcherUtil.allMatches(columnNumberMatchers,
+                        cell.getValueCell().getColumnIndex())
+                && MatcherUtil.allMatches(columnStringMatchers,
+                        CellReference.convertNumToColString(cell.getValueCell().getColumnIndex()));
+    }
 }
