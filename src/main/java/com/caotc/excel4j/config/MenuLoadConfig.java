f69196899a640271816ea729a21e3aa679a7c3e3
caotc <250622148@qq.com>
2017-10-17 17:17:44 +0800

    临时保存

--- src/main/java/com/caotc/excel4j/config/MenuLoadConfig.java
+++ src/main/java/com/caotc/excel4j/config/MenuLoadConfig.java
@@ -3,18 +3,55 @@ package com.caotc.excel4j.config;
 import com.caotc.excel4j.constant.LoadType;
 import com.caotc.excel4j.parse.result.Menu;
 import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableSet;
 
 public class MenuLoadConfig {
+  public static class Builder {
+    private MenuConfig menuConfig;
+    private LoadType loadType;
+    private com.google.common.collect.ImmutableSet.Builder<MenuConfig> childrenMenuConfigs=ImmutableSet.builder();
+    private Integer dataNumber;
+    
+    public Builder menuConfig(MenuConfig menuConfig) {
+      this.menuConfig=menuConfig;
+      return this;
+    }
+    
+    public Builder loadType(LoadType loadType) {
+      this.loadType=loadType;
+      return this;
+    }
+    
+    public Builder addChildrenMenuConfig(MenuConfig config) {
+      childrenMenuConfigs.add(config);
+      return this;
+    }
+    
+    public Builder dataNumber(Integer dataNumber) {
+      this.dataNumber=dataNumber;
+      return this;
+    }
+    
+    public MenuLoadConfig build() {
+      return new MenuLoadConfig(this);
+    }
+    
+  }
+  
+  public static Builder builder() {
+    return new Builder();
+  }
+  
   private final MenuConfig menuConfig;
   private final LoadType loadType;
   private final ImmutableCollection<MenuConfig> childrenMenuConfigs;
+  private final Integer dataNumber;
 
-  public MenuLoadConfig(MenuConfig menuConfig, LoadType loadType,
-      ImmutableCollection<MenuConfig> childrenMenuConfigs) {
-    super();
-    this.menuConfig = menuConfig;
-    this.loadType = loadType;
-    this.childrenMenuConfigs = childrenMenuConfigs;
+  public MenuLoadConfig(Builder builder) {
+    menuConfig=builder.menuConfig;
+    loadType=builder.loadType;
+    childrenMenuConfigs=builder.childrenMenuConfigs.build();
+    dataNumber=builder.dataNumber;
   }
 
   public void load(Menu menu) {
@@ -22,7 +59,7 @@ public class MenuLoadConfig {
       loadType.loadChildren(menu);
     }
   }
-  
+
   public MenuConfig getMenuConfig() {
     return menuConfig;
   }
@@ -34,5 +71,9 @@ public class MenuLoadConfig {
   public ImmutableCollection<MenuConfig> getChildrenMenuConfigs() {
     return childrenMenuConfigs;
   }
-  
+
+  public Integer getDataNumber() {
+    return dataNumber;
+  }
+
 }
