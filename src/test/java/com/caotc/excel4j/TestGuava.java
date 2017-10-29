27afe75956656a12ef4a5e3324993dc8e7ac6d62
caotc <250622148@qq.com>
2017-10-29 21:50:35 +0800

    临时保存

--- src/test/java/com/caotc/excel4j/TestGuava.java
+++ src/test/java/com/caotc/excel4j/TestGuava.java
@@ -2,10 +2,14 @@ package com.caotc.excel4j;
 
 import java.util.Collection;
 import java.util.List;
-import javax.annotation.Nullable;
+import java.util.Map;
+import java.util.Set;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.base.Predicates;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
 import com.google.common.reflect.TypeToken;
 
 class Table<T> {
@@ -13,27 +17,56 @@ class Table<T> {
     TypeToken<T> typeToken = new TypeToken<T>(getClass()) {};
     System.out.println(typeToken);
     System.out.println(typeToken.getRawType());
-    TypeToken<?> token=typeToken.resolveType(typeToken.getRawType().getTypeParameters()[0]);
-    System.out.println(token);
+    // TypeToken<?> token=typeToken.resolveType(typeToken.getRawType().getTypeParameters()[0]);
+    // System.out.println(token);
     return null;
   }
 }
 
+
+class AAA<T> {
+  public Set<T> values;
+
+  @Override
+  public String toString() {
+    return "AAA [values=" + values + "]";
+  }
+}
+
+
+class BBB {
+  public final String name="name";
+  public String type;
+}
+
+
 public class TestGuava {
-  public static void main(String[] args) throws Exception{
-//    Table<List<String>> table=new Table<List<String>>() {};
-//    List<String> n=table.get();
+  public static void main(String[] args) throws Exception {
+    // Table<List<String>> table=new Table<List<String>>();
+    // List<String> n=table.get();
+
+    JSONObject jsonObject = new JSONObject();
+    JSONArray jsonArray = new JSONArray();
+    jsonArray.fluentAdd(new JSONObject().fluentPut("name", "a").fluentPut("type", "region"))
+        .fluentAdd(new JSONObject().fluentPut("name", "b").fluentPut("type", "city"));
+    jsonObject.put("values", jsonArray);
+    System.out.println(jsonObject.toJavaObject(AAA.class));
+    Collection<AAA> c=new JSONArray().fluentAdd(jsonObject).toJavaObject(Collection.class);
+    System.out.println(c);
+    AAA aaa=c.iterator().next();
     
-//    System.out.println(int[][][].class.equals(int[].class));
-    
-    Class<?> type=void.class;
-    System.out.println(type.isInterface());
-    System.out.println(type.isLocalClass());
-    System.out.println(type.isMemberClass());
-    System.out.println(type.isSynthetic());
-    
-    TypeToken token=TypeToken.of(void.class);
-    System.out.println(token.isPrimitive());
+    System.out.println(jsonObject.toJavaObject(Map.class).get("values"));
+    Multimap<String,Object> mmap=jsonObject.toJavaObject(Multimap.class);
+    System.out.println(mmap.get("values"));
+    // System.out.println(int[][][].class.equals(int[].class));
+    // Class<?> type=void.class;
+    // System.out.println(type.isInterface());
+    // System.out.println(type.isLocalClass());
+    // System.out.println(type.isMemberClass());
+    // System.out.println(type.isSynthetic());
+    //
+    // TypeToken token=TypeToken.of(void.class);
+    // System.out.println(token.isPrimitive());
   }
 
   public static void whenFilterWithCollections2_thenFiltered() {
