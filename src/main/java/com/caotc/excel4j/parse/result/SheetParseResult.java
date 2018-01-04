e6b5ab186a7eb081aa6240bde0a51abe2ce12afe
caotc <250622148@qq.com>
2018-01-05 00:52:52 +0800

    临时保存

--- src/main/java/com/caotc/excel4j/parse/result/SheetParseResult.java
+++ src/main/java/com/caotc/excel4j/parse/result/SheetParseResult.java
@@ -1,26 +1,17 @@
 package com.caotc.excel4j.parse.result;
 
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.util.CellRangeAddress;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.caotc.excel4j.config.MenuConfig;
+import org.apache.poi.ss.usermodel.Workbook;
 import com.caotc.excel4j.config.SheetConfig;
+import com.caotc.excel4j.config.WorkbookConfig;
 import com.caotc.excel4j.parse.error.SheetError;
-import com.caotc.excel4j.util.ExcelUtil;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 
 public class SheetParseResult {
+  public static SheetParseResult parse() {
+    return new SheetParseResult();
+  }
+  
   private Sheet sheet;
   private SheetConfig sheetConfig;
   private List<SheetError> sheetErrors;
--- src/main/java/com/caotc/excel4j/parse/result/WorkbookParseResult.java
+++ src/main/java/com/caotc/excel4j/parse/result/WorkbookParseResult.java
@@ -4,15 +4,40 @@ import java.util.List;
 import org.apache.poi.ss.usermodel.Workbook;
 import com.caotc.excel4j.config.WorkbookConfig;
 import com.caotc.excel4j.parse.error.WorkbookError;
+import com.google.common.collect.ImmutableList;
 
 public class WorkbookParseResult {
-  public static WorkbookParseResult parse() {
-    return new WorkbookParseResult();
+  public static WorkbookParseResult parse(Workbook workbook, WorkbookConfig workbookConfig) {
+    return new WorkbookParseResult(workbook, workbookConfig);
   }
-  
-  private Workbook workbook;
-  private WorkbookConfig workbookConfig;
-  private List<WorkbookError> workbookErrors;
-  private List<SheetParseResult> sheetParseResults;
 
+  private final Workbook workbook;
+  private final WorkbookConfig workbookConfig;
+  private final ImmutableList<WorkbookError> workbookErrors;
+  private final ImmutableList<SheetParseResult> sheetParseResults;
+
+  public WorkbookParseResult(Workbook workbook, WorkbookConfig workbookConfig) {
+    super();
+    this.workbook = workbook;
+    this.workbookConfig = workbookConfig;
+    // TODO
+    workbookErrors = null;
+    sheetParseResults = null;
+  }
+
+  public Workbook getWorkbook() {
+    return workbook;
+  }
+
+  public WorkbookConfig getWorkbookConfig() {
+    return workbookConfig;
+  }
+
+  public List<WorkbookError> getWorkbookErrors() {
+    return workbookErrors;
+  }
+
+  public List<SheetParseResult> getSheetParseResults() {
+    return sheetParseResults;
+  }
 }
