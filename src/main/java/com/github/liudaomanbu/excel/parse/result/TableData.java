57b847bba5cb5b58e97edf440e7df59443c2afbe
caotc <250622148@qq.com>
2018-01-26 23:11:06 +0800

    修复没有unfixedDataMenu时,即使有fixedDataMenu也没有数据的bug

--- src/main/java/com/github/liudaomanbu/excel/parse/result/TableData.java
+++ src/main/java/com/github/liudaomanbu/excel/parse/result/TableData.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import com.github.liudaomanbu.excel.config.TableDataConfig;
@@ -11,6 +12,7 @@ import com.github.liudaomanbu.excel.parse.error.ValidationError;
 import com.github.liudaomanbu.excel.util.ExcelUtil;
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -46,7 +48,7 @@ public class TableData<T> {
   }
 
   private static final Joiner JOINER = Joiner.on("").skipNulls();
-  
+
   private final Table<T> table;
   private final TableDataConfig<T> config;
   private final ImmutableList<Data> datas;
@@ -57,6 +59,12 @@ public class TableData<T> {
     this.config = table.getConfig().getDataConfig();
     List<Map<Menu<T>, StandardCell>> menuTodatas = Lists.newArrayList();
     ImmutableList<Menu<T>> menus = table.getDataMenus().collect(ImmutableList.toImmutableList());
+
+    ImmutableMap<Menu<T>, StandardCell> menuToFixedDatas =
+        menus.stream().filter(Menu::isSingleDataMenu).collect(ImmutableMap.toImmutableMap(
+            Function.identity(), menu -> Iterables.getOnlyElement(menu.getData().getValueCells())));
+    menuTodatas.add(menuToFixedDatas);
+
     menus.stream().filter(Menu::isUnFixedDataMenu).forEach(menu -> {
       ImmutableList<StandardCell> valueCells = menu.getData().getValueCells();
       for (int j = 0; j < valueCells.size(); j++) {
@@ -66,16 +74,12 @@ public class TableData<T> {
         if (j < menuTodatas.size()) {
           map = menuTodatas.get(j);
         } else {
-          map = Maps.newHashMap();
+          map = Maps.newHashMap(menuToFixedDatas);
           menuTodatas.add(map);
         }
         map.put(menu, valueCell);
       }
     });
-
-    menus.stream().filter(Menu::isSingleDataMenu)
-        .forEach(menu -> menuTodatas.forEach(menuTodata -> menuTodata.put(menu,
-            Iterables.getOnlyElement(menu.getData().getValueCells()))));
 
     datas = menuTodatas.stream().map(Data::new).collect(ImmutableList.toImmutableList());
 
@@ -89,8 +93,8 @@ public class TableData<T> {
 
     Stream<ValidationError<TableData<T>>> tableDataMatcherErrors = Optional.ofNullable(config)
         .map(TableDataConfig::getValidators).orElse(ImmutableList.of()).stream()
-        .flatMap(validator -> datas.stream()
-            .map(map -> validator.validate(map)).flatMap(Collection::stream))
+        .flatMap(validator -> datas.stream().map(map -> validator.validate(map))
+            .flatMap(Collection::stream))
         .map(error -> new ValidationError<>(this, error.getMessage()));
 
     this.errors = Stream.concat(menuMatcherErrors, tableDataMatcherErrors)
