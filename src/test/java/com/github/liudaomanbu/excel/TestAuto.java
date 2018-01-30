7d11fb57a3e09b0e2186cbf20fb40372dbd99cff
caotc <250622148@qq.com>
2018-01-31 00:27:40 +0800

    为了避免报错,注释掉这部分代码

--- src/test/java/com/github/liudaomanbu/excel/TestAuto.java
+++ src/test/java/com/github/liudaomanbu/excel/TestAuto.java
@@ -1,21 +1,50 @@
 package com.github.liudaomanbu.excel;
 
 import com.google.auto.value.AutoValue;
-
-@AutoValue
-abstract class Animal {
-//  static Animal create(String name, int numberOfLegs) {
-//    // See "How do I...?" below for nested classes.
-//    return new AutoValue_Animal(name, numberOfLegs);
+//
+//@AutoValue
+//abstract class Animal {
+//  abstract String name();
+//  abstract int numberOfLegs();
+//  abstract Person person();
+//
+//  public static Animal create(String name, int numberOfLegs, Person person) {
+//    return builder()
+//        .name(name)
+//        .numberOfLegs(numberOfLegs)
+//        .person(person)
+//        .build();
 //  }
-
-  abstract String name();
-  abstract int numberOfLegs();
-
-  public static Animal create(String name, int numberOfLegs) {
-    return new AutoValue_Animal(name, numberOfLegs);
-  }
-}
+//
+//  public static Builder builder() {
+//    return new AutoValue_Animal.Builder();
+//  }
+//
+//
+//  @AutoValue.Builder
+//  public abstract static class Builder {
+//
+//    public abstract Builder name(String name);
+//
+//    public abstract Builder numberOfLegs(int numberOfLegs);
+//
+//    public abstract Builder person(Person person);
+//
+//    public abstract Animal build();
+//  }
+//}
+//
+//class Person{
+//  private final Animal Animal;
+//
+//  public Person(com.github.liudaomanbu.excel.Animal animal) {
+//    Animal = animal;
+//  }
+//}
 public class TestAuto {
 
+  public static void main(String[] args) {
+//    Animal animal=Animal.create("abc",4,null);
+//    System.out.println(animal);
+  }
 }
